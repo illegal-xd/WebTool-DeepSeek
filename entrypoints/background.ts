e306c1d0121876fbe427085f2c8110a720a487e7
w18826555761 <zhangxudong_a@aspirecn.com>
2026-05-20 18:23:18 +0800

    feat: Update Operation Mode

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -120,7 +120,7 @@ async function handleMessage(
       return getActivePreset();
 
     case 'GET_CONFIG':
-      return { version: '0.1.1' };
+      return { version: '0.2.0' };
 
     case 'GET_MODEL_TYPE':
       return getModelType();
--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
           </h1>
         </div>
         <span className="text-[11px] px-2 py-0.5 rounded-full" style={{ color: 'var(--ds-text-tertiary)', background: 'var(--ds-surface)' }}>
-          v0.1.1
+          v0.2.0
         </span>
       </header>
 
